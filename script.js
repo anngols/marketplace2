b419545f94f0d83adc773fae9b7a0c8393970754
anngols <anna.radenko123@gmail.com>
2023-12-02 19:40:21 +0200

    marketplace2 project

---
+++ script.js
@@ -0,0 +1,28 @@
+let productsGrid = document.querySelector(".product-grid")
+let productsArray = [];
+let xhr = new XMLHttpRequest()
+let url = "https://my-json-server.typicode.com/anngols/marketplace2/db"
+
+xhr.open("GET", url)
+xhr.responseType = "json"
+xhr.onload = ()=>{
+    productsArray = xhr.response.products
+    productsGrid.innerHTML = ""
+    productsArray.forEach(p => {
+        productsArray.push(p)
+        let pElem = document.createElement("div")
+        pElem.classList.add("product")
+        pElem.innerHTML = `
+        <img src="${p.img}" alt="">
+                <h2 class="product-name">${p.name}</h2>
+                <p class="product-desc">${p.description}</p>
+                <a href="profile/profile.html?id=${p.author_id}">Seller product</a>
+                <div class="price-bar">
+                    <p class="product-price">${p.price}</p>
+                    <button onclick="addProductToCart(${p.id})"><i class="da-solid fa-plus"></i></button>
+                </div>`
+
+                productsGrid.append(pElem)
+    });
+}
+xhr.send()
